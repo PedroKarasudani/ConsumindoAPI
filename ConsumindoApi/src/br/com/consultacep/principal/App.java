117f1b0b3c9e5f980e035e93262369219ff95992
Pedro K. L <pedro.karasudani@hotmail.com>
2023-06-26 20:11:41 -0300

    Scanner

--- ConsumindoApi/src/br/com/consultacep/principal/App.java
+++ ConsumindoApi/src/br/com/consultacep/principal/App.java
@@ -5,11 +5,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.print("Digite o cep: ");
+        Scanner leitura = new Scanner(System.in);
+        String cep = leitura.nextLine();
 
-        String endereco = "https://viacep.com.br/ws/86046460/json/";
+        String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
