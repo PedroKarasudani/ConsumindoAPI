1c513042c27303ccb2f4e260156174919f41666f
Pedro K. L <pedro.karasudani@hotmail.com>
2023-06-26 20:13:13 -0300

    Gson + FileWriter

--- ConsumindoApi/src/br/com/consultacep/principal/App.java
+++ ConsumindoApi/src/br/com/consultacep/principal/App.java
@@ -1,11 +1,16 @@
 package br.com.consultacep.principal;
 
+import java.io.FileWriter;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Scanner;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -15,11 +20,16 @@ public class App {
 
         String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
 
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
         String json = response.body();
-        System.out.println(json);
+        
+        FileWriter escrita = new FileWriter("cep.json");
+        escrita.write(gson.toJson(json));
+        escrita.close();
     }
 }
